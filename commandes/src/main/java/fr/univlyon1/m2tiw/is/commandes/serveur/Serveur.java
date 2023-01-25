28df2d527c99763a885fdcbe5a24c1a0357ca5fb
KevinFroissart <kevin.froissart69@gmail.com>
2023-01-25 23:02:25 +0100

    Isolation du serveur, routage des controlleurs et de leurs méthodes

--- commandes/src/main/java/fr/univlyon1/m2tiw/is/commandes/serveur/Serveur.java
+++ commandes/src/main/java/fr/univlyon1/m2tiw/is/commandes/serveur/Serveur.java
@@ -1,108 +1,14 @@
 package fr.univlyon1.m2tiw.is.commandes.serveur;
 
-import fr.univlyon1.m2tiw.is.commandes.controller.CommandeController;
-import fr.univlyon1.m2tiw.is.commandes.controller.OptionController;
-import fr.univlyon1.m2tiw.is.commandes.controller.VoitureController;
-import fr.univlyon1.m2tiw.is.commandes.dao.CommandeDAO;
-import fr.univlyon1.m2tiw.is.commandes.dao.CommandeDAOImpl;
 import fr.univlyon1.m2tiw.is.commandes.dao.NotFoundException;
-import fr.univlyon1.m2tiw.is.commandes.dao.OptionDAO;
-import fr.univlyon1.m2tiw.is.commandes.dao.OptionDAOImpl;
-import fr.univlyon1.m2tiw.is.commandes.dao.VoitureDAO;
-import fr.univlyon1.m2tiw.is.commandes.dao.VoitureDAOImpl;
-import fr.univlyon1.m2tiw.is.commandes.model.Commande;
-import fr.univlyon1.m2tiw.is.commandes.model.Option;
-import fr.univlyon1.m2tiw.is.commandes.model.Voiture;
-import fr.univlyon1.m2tiw.is.commandes.services.CommandeCouranteService;
-import fr.univlyon1.m2tiw.is.commandes.services.CommandeCouranteServiceImpl;
-import fr.univlyon1.m2tiw.is.commandes.services.DBAccess;
 import fr.univlyon1.m2tiw.is.commandes.services.EmptyCommandeException;
-import fr.univlyon1.m2tiw.is.commandes.services.GestionCommandeService;
-import fr.univlyon1.m2tiw.is.commandes.services.GestionCommandeServiceImpl;
 import fr.univlyon1.m2tiw.is.commandes.services.InvalidConfigurationException;
-import fr.univlyon1.m2tiw.is.commandes.services.OptionService;
-import fr.univlyon1.m2tiw.is.commandes.services.OptionServiceImpl;
-import fr.univlyon1.m2tiw.is.commandes.services.VoitureService;
-import fr.univlyon1.m2tiw.is.commandes.services.VoitureServiceImpl;
-import org.picocontainer.DefaultPicoContainer;
-import org.picocontainer.MutablePicoContainer;
-import org.picocontainer.behaviors.Caching;
-import org.picocontainer.parameters.ComponentParameter;
 
 import java.sql.SQLException;
-import java.util.Collection;
+import java.util.Map;
 
-public class Serveur {
+public interface Serveur {
 
-    private final VoitureController voitureController;
-    private final OptionController optionController;
-    private final CommandeController commandeController;
+    Object processRequest(String commande, Map<String, Object> parametres) throws SQLException, EmptyCommandeException, NotFoundException, InvalidConfigurationException;
 
-
-    public Serveur() {
-        MutablePicoContainer pico = new DefaultPicoContainer(new Caching());
-        pico.addComponent(CommandeDAO.class, CommandeDAOImpl.class);
-        pico.addComponent(OptionDAO.class, OptionDAOImpl.class);
-        pico.addComponent(VoitureDAO.class, VoitureDAOImpl.class);
-
-        pico.addComponent(VoitureService.class, VoitureServiceImpl.class,
-                new ComponentParameter(VoitureDAO.class),
-                new ComponentParameter(OptionDAO.class)
-        );
-        pico.addComponent(OptionService.class, OptionServiceImpl.class,
-                new ComponentParameter(OptionDAO.class)
-        );
-        pico.addComponent(GestionCommandeService.class, GestionCommandeServiceImpl.class,
-                new ComponentParameter(OptionService.class),
-                new ComponentParameter(VoitureService.class),
-                new ComponentParameter(CommandeCouranteService.class),
-                new ComponentParameter(CommandeDAO.class));
-        pico.addComponent(CommandeCouranteService.class, CommandeCouranteServiceImpl.class,
-                new ComponentParameter(VoitureService.class),
-                new ComponentParameter(CommandeDAO.class));
-        pico.addComponent(DBAccess.class);
-
-        pico.addComponent(VoitureController.class, new ComponentParameter(VoitureService.class));
-        pico.addComponent(OptionController.class, new ComponentParameter(OptionService.class));
-        pico.addComponent(CommandeController.class,
-                new ComponentParameter(GestionCommandeService.class),
-                new ComponentParameter(CommandeCouranteService.class)
-        );
-
-        voitureController = pico.getComponent(VoitureController.class);
-        optionController = pico.getComponent(OptionController.class);
-        commandeController = pico.getComponent(CommandeController.class);
-    }
-
-    public Collection<Option> getAllOptions() throws SQLException {
-        return optionController.getAllOptions();
-    }
-
-    public Voiture creerVoiture(String modele) throws SQLException {
-        return voitureController.creerVoiture(modele);
-    }
-
-    public void ajouterConfiguration(Long voitureId, Option option) throws SQLException, NotFoundException {
-        voitureController.ajouterConfiguration(voitureId, option);
-    }
-
-    public void supprimerConfiguration(Long voitureId, Option option) throws SQLException, NotFoundException, InvalidConfigurationException {
-        voitureController.supprimerConfiguration(voitureId, option);
-    }
-
-    public void ajouterVoiture(Long voitureId) throws SQLException, NotFoundException {
-        commandeController.ajouterVoiture(voitureId);
-    }
-
-    public void supprimerVoiture(Long voitureId) throws SQLException, NotFoundException {
-        commandeController.supprimerVoiture(voitureId);
-    }
-
-    public long validerCommandeCourante() throws EmptyCommandeException, SQLException, NotFoundException {
-        return commandeController.validerCommandeCourante();
-    }
-
-    public Commande getCommande(Long id) throws SQLException, NotFoundException {
-        return commandeController.getCommande(id);
-    }
 }
---
+++ commandes/src/main/java/fr/univlyon1/m2tiw/is/commandes/serveur/ServeurImpl.java
@@ -0,0 +1,106 @@
+package fr.univlyon1.m2tiw.is.commandes.serveur;
+
+import fr.univlyon1.m2tiw.is.commandes.controller.CommandeController;
+import fr.univlyon1.m2tiw.is.commandes.controller.OptionController;
+import fr.univlyon1.m2tiw.is.commandes.controller.VoitureController;
+import fr.univlyon1.m2tiw.is.commandes.dao.CommandeDAO;
+import fr.univlyon1.m2tiw.is.commandes.dao.CommandeDAOImpl;
+import fr.univlyon1.m2tiw.is.commandes.dao.NotFoundException;
+import fr.univlyon1.m2tiw.is.commandes.dao.OptionDAO;
+import fr.univlyon1.m2tiw.is.commandes.dao.OptionDAOImpl;
+import fr.univlyon1.m2tiw.is.commandes.dao.VoitureDAO;
+import fr.univlyon1.m2tiw.is.commandes.dao.VoitureDAOImpl;
+import fr.univlyon1.m2tiw.is.commandes.services.CommandeCouranteService;
+import fr.univlyon1.m2tiw.is.commandes.services.CommandeCouranteServiceImpl;
+import fr.univlyon1.m2tiw.is.commandes.services.DBAccess;
+import fr.univlyon1.m2tiw.is.commandes.services.EmptyCommandeException;
+import fr.univlyon1.m2tiw.is.commandes.services.GestionCommandeService;
+import fr.univlyon1.m2tiw.is.commandes.services.GestionCommandeServiceImpl;
+import fr.univlyon1.m2tiw.is.commandes.services.InvalidConfigurationException;
+import fr.univlyon1.m2tiw.is.commandes.services.OptionService;
+import fr.univlyon1.m2tiw.is.commandes.services.OptionServiceImpl;
+import fr.univlyon1.m2tiw.is.commandes.services.VoitureService;
+import fr.univlyon1.m2tiw.is.commandes.services.VoitureServiceImpl;
+import org.picocontainer.DefaultPicoContainer;
+import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.behaviors.Caching;
+import org.picocontainer.parameters.ComponentParameter;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+public class ServeurImpl implements Serveur {
+
+    private final VoitureController voitureController;
+    private final OptionController optionController;
+    private final CommandeController commandeController;
+
+
+    public ServeurImpl() {
+        MutablePicoContainer pico = new DefaultPicoContainer(new Caching());
+        pico.addComponent(CommandeDAO.class, CommandeDAOImpl.class);
+        pico.addComponent(OptionDAO.class, OptionDAOImpl.class);
+        pico.addComponent(VoitureDAO.class, VoitureDAOImpl.class);
+
+        pico.addComponent(VoitureService.class, VoitureServiceImpl.class,
+                new ComponentParameter(VoitureDAO.class),
+                new ComponentParameter(OptionDAO.class)
+        );
+        pico.addComponent(OptionService.class, OptionServiceImpl.class,
+                new ComponentParameter(OptionDAO.class)
+        );
+        pico.addComponent(GestionCommandeService.class, GestionCommandeServiceImpl.class,
+                new ComponentParameter(OptionService.class),
+                new ComponentParameter(VoitureService.class),
+                new ComponentParameter(CommandeCouranteService.class),
+                new ComponentParameter(CommandeDAO.class));
+        pico.addComponent(CommandeCouranteService.class, CommandeCouranteServiceImpl.class,
+                new ComponentParameter(VoitureService.class),
+                new ComponentParameter(CommandeDAO.class));
+        pico.addComponent(DBAccess.class);
+
+        pico.addComponent(VoitureController.class, new ComponentParameter(VoitureService.class));
+        pico.addComponent(OptionController.class, new ComponentParameter(OptionService.class));
+        pico.addComponent(CommandeController.class,
+                new ComponentParameter(GestionCommandeService.class),
+                new ComponentParameter(CommandeCouranteService.class)
+        );
+
+        voitureController = pico.getComponent(VoitureController.class);
+        optionController = pico.getComponent(OptionController.class);
+        commandeController = pico.getComponent(CommandeController.class);
+
+        voitureController.start();
+        optionController.start();
+        commandeController.start();
+    }
+
+    public Object processRequest(String commande, Map<String, Object> parametres) throws SQLException, EmptyCommandeException, NotFoundException, InvalidConfigurationException {
+        String[] commandes = commande.toLowerCase().split("\\.");
+        switch (commandes[0]) {
+            case "commandecontroller":
+                return commandeController.process(commandes[1], parametres);
+            case "optioncontroller":
+                return optionController.process(commandes[1], parametres);
+            case "voiturecontroller":
+                return voitureController.process(commandes[1], parametres);
+            default:
+                return null;
+        }
+    }
+
+    // Provisoire
+    public VoitureController getVoitureController() {
+        return voitureController;
+    }
+
+    // Provisoire
+    public OptionController getOptionController() {
+        return optionController;
+    }
+
+    // Provisoire
+    public CommandeController getCommandeController() {
+        return commandeController;
+    }
+}
